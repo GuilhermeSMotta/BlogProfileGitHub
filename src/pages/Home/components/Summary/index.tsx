b7c80d8b2e7cac98797cb554e7b640c5373627c6
Guilherme Motta <guilhermesm221@outlook.com>
2025-04-02 20:57:30 -0300

    Atualizar acesso de dados

--- src/pages/Home/components/Summary/index.tsx
+++ src/pages/Home/components/Summary/index.tsx
@@ -1,9 +1,45 @@
+import { useEffect, useState } from "react";
 import { SummaryAnchors, SummaryContainer, SummaryHeader } from "./styles";
 
 import { ArrowUpRight, Buildings, GithubLogo, Users } from "phosphor-react";
 
+import { api } from "../../../../lib/api";
+
+interface User {
+  avatar_url: string;
+  bio: string;
+  company: string;
+  followers: number;
+  login: string;
+  name: string;
+  html_url: string;
+
+}
 
 export function Summary() {
+  /*
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/GuilhermeSMotta").then((response) => {
+      response.json().then((res) => {
+        setUser(res);
+      });
+    }).catch((error) => {
+      console.log(error);
+    });
+  }, []);
+  */
+
+  const [user, setUser] = useState<User | null>(null); 
+  async function fetchUser() {
+    const response = await api.get<User>("/users/GuilhermeSMotta");
+    setUser(response.data);
+  }
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
 
   // "https://api.github.com/users", "/lucaspedronet"
   // "https://api.github.com/search"
@@ -11,30 +47,30 @@ export function Summary() {
 
   return (
     <SummaryContainer>
-      <img src="../src/assets/avatar.JPG" />
+      <img src={user?.avatar_url} />
       <section>
         <SummaryHeader>
-          <h1>Lucas Pedro</h1>
-          <a href="#" target="_blank">
+          <h1>{user?.name}</h1>
+          <a href={user?.} target="_blank">
             GITHUB
             <ArrowUpRight size={12} />
           </a>
         </SummaryHeader>
-        <p>Software Engineering. developer at NodeJS, ReactJS, React Native, Electron.</p>
+        <p>{user?.bio}</p>
         <SummaryAnchors>
           <div>
             <GithubLogo size={18} />
-            <span>lucaspedronet</span>
+            <span>{user?.login}</span>
           </div>
 
           <div>
             <Buildings size={18} />
-            <span>Paraná Banco</span>
+            <span>{user?.company}</span>
           </div>
 
           <div>
             <Users size={18} />
-            <span>47</span>
+            <span>{user?.followers}</span>
           </div>
         </SummaryAnchors>
       </section>
